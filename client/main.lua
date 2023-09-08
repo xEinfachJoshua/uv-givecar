ee0f9ab3bedc38ba6e66abd6bc8bc4fada6fbdc1
xEinfachJoshua <144360231+xEinfachJoshua@users.noreply.github.com>
2023-09-08 22:59:09 +0200

    Final Version

--- client/main.lua
+++ client/main.lua
@@ -1,13 +1,15 @@
+ESX = exports['es_extended']:getSharedObject()
 RegisterCommand(config.command, function(source, args)
-    local id = args[1]
-    local playerPed = PlayerPedId()
-    local coords = GetEntityCoords(playerPed)
-    local heading = GetEntityHeading(playerPed)
-    local playerServerID = GetPlayerServerId(PlayerId())
-    if IsPedInAnyVehicle(playerPed, false) then
-        local vehicle = GetVehiclePedIsIn(playerPed, false)
-        TriggerServerEvent("uv-givecar:checkowner", playerServerID, vehicle, destinationId)
-    else
-        TriggerEvent("esx:ShowNotification", "~r~Du bist in keinem Fahrzeug!")
-    end
+        local playerPed = PlayerPedId()
+        local coords = GetEntityCoords(playerPed)
+        local heading = GetEntityHeading(playerPed)
+        local playerServerID = GetPlayerServerId(PlayerId())
+        local destinationId = tonumber(args[1])
+        if IsPedInAnyVehicle(playerPed, false) then
+            local vehicle = GetVehiclePedIsIn(playerPed, false)
+            local vehicleProps = ESX.Game.GetVehicleProperties(vehicle)
+            TriggerServerEvent("uv-givecar:givecar", playerServerID, vehicleProps, destinationId)
+        else
+            TriggerEvent("esx:ShowNotification", "~r~Du bist in keinem Fahrzeug!")
+        end
 end, false)
--- server/main.lua
+++ server/main.lua
@@ -1,25 +1,25 @@
+ESX = exports['es_extended']:getSharedObject()
 RegisterServerEvent('uv-givecar:givecar')
 AddEventHandler('uv-givecar:givecar', function(source, vehicle, destinationId)
-    local plate = GetVehicleNumberPlateText(vehicle)
     local xPlayer = ESX.GetPlayerFromId(source)
     local xTarget = ESX.GetPlayerFromId(destinationId)
-    MySQL.prepere("SELECT * FROM onwed_vehicles WHERE plate = @plate", {
-        ['@plate'] = plate
-    }, function(result)
-        result = json.encode(result)
-        result = json.decode(result)
-        if result["owner"] == xPlayer.identifier then
-            MySQL.update("UPDATE owned_vehicles SET owner = @owner WHERE plate = @plate", {
-                ['@owner'] = xTarget.identifier,
-                ['@plate'] = plate
-            })
-            TriggerClientEvent("esx:ShowNotification", source, "Du hast das Fahrzeug mit dem Kennzeichen ~g~" .. plate .. "~w~ an ~g~" .. xTarget.name .. "~w~ gegeben!")
-            TriggerClientEvent("esx:ShowNotification", destinationId, "Du hast das Fahrzeug mit dem Kennzeichen ~g~" .. plate .. "~w~ von ~g~" .. xPlayer.name .. "~w~ bekommen!")
-        else
-            TriggerClientEvent("esx:ShowNotification", source, "Du bist nicht der Besitzer des Fahrzeugs mit dem Kennzeichen ~g~" .. plate .. "~w~!")
-        end
-    end)
-    
+    local plate = vehicle.plate
+    local response = MySQL.prepare.await('SELECT * FROM `owned_vehicles` WHERE `plate` = ?', {
+        plate
+    })
+    if xTarget == nil then
+        TriggerClientEvent("esx:ShowNotification", source, "~r~Dieser Spieler ist nicht online!")
+        return
+    else
+
+    if response["owner"] == xPlayer.identifier then
+        MySQL.update.await('UPDATE `owned_vehicles` SET `owner` = ? WHERE `plate` = ?', {
+            xTarget.identifier,
+            plate
+        })
+        TriggerClientEvent("esx:ShowNotification", source, "~g~Du hast das Fahrzeug erfolgreich an "..xTarget.name.." gegeben!")
+        TriggerClientEvent("esx:ShowNotification", destinationId, "~g~Du hast ein Fahrzeug erhalten!")
+    else
+        TriggerClientEvent("esx:ShowNotification", source, "~r~Du bist nicht der Besitzer dieses Fahrzeugs!")
+    end
 end)
-
-
